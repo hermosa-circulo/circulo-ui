e99464110dfff8e24e4f8c5868193ddb9684815e
root <root@ip-172-31-24-86.ap-northeast-1.compute.internal>
2017-10-25 15:50:36 +0000

    allow specify domain

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 set -e
-
-/resources/node_modules/.bin/ng serve --host 0.0.0.0  --port 80 --open
+git clone https://github.com/hermosa-circulo/docs.git /docs
+if [ -e /resources/src/assets/docs ]; then
+    rm -rf /resource/src/assets/docs
+fi
+mv /docs /resources/src/assets/docs
+/resources/node_modules/.bin/ng serve --host 0.0.0.0  --port 80 --open --public ${DOMAIN_NAME}
